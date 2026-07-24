1a9aa18e49f17cebc04e5a184fd04c08472f57bf
GMY <3294447364@qq.com>
2026-07-25 02:51:03 +0800

    fix: windres toolchain — C wrapper (winds.c) for full-path windres invocation

---
+++ src-tauri/winds.c
@@ -0,0 +1,9 @@
+#include <process.h>
+int main(int argc, char **argv) {
+    const char *args[256];
+    args[0] = "F:\\Coding\\c\\mingw64\\mingw64\\bin\\windres.exe";
+    int i;
+    for (i = 1; i < argc && i < 255; i++) args[i] = argv[i];
+    args[i] = 0;
+    return _spawnv(_P_WAIT, args[0], args);
+}
